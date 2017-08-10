45a47c01211e53ca70d558c034ac2442b24ccfe6
Jerome <jscar216@gmail.com>
2017-08-10 09:48:26 -0400

    added functions

---
+++ jqueryjsonapis/Scripts/myjQuery.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    $("#findButton").click(function () {
+        var searchTerm = $("#veggieInput").val();
+        searchTerm = searchTerm.toLowerCase();
+        var listItems = $("li");
+        listItems.each(function () {
+            var words = $(this).text().toLowerCase();
+            if (!words.includes(searchTerm)) {
+                $(this).hide("slow");
+            }
+            if (words.includes(searchTerm)) {
+                $(this).show("slow");
+            }
+        });
+    });
+    //User should be able to click the View all button and all the recipes are shown.
+    $("#viewAllButton").click(function () {
+        var listItems = $("li");
+        listItems.each(function () {
+            $(this).show("fast");
+        })
+    })
+
+    //
+
+});
+    
